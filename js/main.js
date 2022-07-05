3cbc6c98ec112ab25245a7f1318f339003309b58
Елизавета Яценко <sipengodev@gmail.com>
2022-07-05 23:30:30 +0300

    Правки после проверки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,2 @@
-import {render} from './render.js';
-render();
-
-//import {createPost, SIMILAR_CREATE_POST} from './data.js';
-//Array.from({length: SIMILAR_CREATE_POST}, createPost);
+import {renderPost} from './render.js';
+renderPost();
--- js/render.js
+++ js/render.js
@@ -8,38 +8,83 @@ const popupType = {
   hotel: 'Отель'
 };
 
-
-const getRenderPost = (post) => {
+/**
+ * Преобразовать новость в HTML элемент (Node)
+ * @param post
+ * @returns {Node}
+ */
+const convertPostToHtmlElement = (post) => {
   const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
   const similarElement = cardTemplate.cloneNode(true);
-  similarElement.querySelector('.popup__avatar').src = post.author.avatar;
-  similarElement.querySelector('.popup__title').textContent = post.offer.title;
-  similarElement.querySelector('.popup__text--address').textContent = post.offer.address;
-  similarElement.querySelector('.popup__text--price').textContent = `${post.offer.price  } р/ночь`;
-  similarElement.querySelector('.popup__type').textContent = popupType[post.offer.type] || '-';
-  similarElement.querySelector('.popup__text--capacity').textContent = `${post.offer.rooms  } комнаты для ${  post.offer.guests  } гостей`;
-  similarElement.querySelector('.popup__text--time').textContent = `Заезд после ${  post.offer.checkin  } выезд до ${  post.offer.checkout}`;
-  FEATURES.forEach((feature) => {
-    if (!post.offer.features.includes(feature)) {
-      similarElement.querySelector(`.popup__feature--${  feature}`).classList.add('hidden');
-    }
-  });
-  //similarElement.querySelectorAll('.popup__feature').textContent =post.offer.features;
+  similarElement.querySelector('.popup__text--capacity').textContent = `${post.offer.rooms} комнаты для ${post.offer.guests} гостей`;
+  similarElement.querySelector('.popup__text--time').textContent = `Заезд после ${post.offer.checkin} выезд до ${post.offer.checkout}`;
 
-  similarElement.querySelector('.popup__description').textContent = post.offer.description;
-  similarElement.querySelector('.popup__photo').src = post.offer.photos;
+  if (post.author.avatar) {
+    similarElement.querySelector('.popup__avatar').src = post.author.avatar;
+  } else {
+    post.author.avatar.classList.add('hidden');
+  }
+
+  if (post.offer.features) {
+    FEATURES.forEach((feature) => {
+      if (!post.offer.features.includes(feature)) {
+        similarElement.querySelector(`.popup__feature--${feature}`).classList.add('hidden');
+      }
+    });
+  }
+
+  if (post.offer.title) {
+    similarElement.querySelector('.popup__title').textContent = post.offer.title;
+  } else {
+    post.offer.title.classList.add('hidden');
+  }
+
+  if (post.offer.price) {
+    similarElement.querySelector('.popup__text--price').textContent = `${post.offer.price} р/ночь`;
+  } else {
+    post.offer.price.classList.add('hidden');
+  }
+
+  if (post.offer.description) {
+    similarElement.querySelector('.popup__description').textContent = post.offer.description;
+  } else {
+    post.offer.description.classList.add('hidden');
+  }
+
+  if (post.offer.address) {
+    similarElement.querySelector('.popup__text--address').textContent = post.offer.address;
+  } else {
+    post.offer.address.classList.add('hidden');
+  }
+
+  if (post.offer.type) {
+    similarElement.querySelector('.popup__type').textContent = popupType[post.offer.type];
+  } else {
+    post.offer.type.classList.add('hidden');
+  }
+
+  if (post.offer.photos) {
+    similarElement.querySelector('.popup__photo').remove();
+    post.offer.photos.forEach((img) => {
+      const photoBlock = document.querySelector('#card').content.querySelector('.popup__photo').cloneNode(true);
+      photoBlock.src = img;
+      similarElement.querySelector('.popup__photos').appendChild(photoBlock);
+    });
+  } else {
+    post.offer.photos.classList.add('hidden');
+  }
   return similarElement;
 };
 
-
-const render = () => {
+/**
+ * Метод для отрисовки объявление на странице
+ */
+const renderPost = () => {
   const mapCanvas = document.querySelector('.map__canvas');
-
   const SIMILAR_POST = createPost();
-  //const similarPostFragment = document.createDocumentFragment();
-  mapCanvas.appendChild(getRenderPost(SIMILAR_POST));
+  mapCanvas.appendChild(convertPostToHtmlElement(SIMILAR_POST));
 };
 
 
-export {render};
+export {renderPost};
 
